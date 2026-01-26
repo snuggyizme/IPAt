0790a54cd3226f01a29428dd4106e1a2b99939b8
Elliot Thurlow <snuggyizme@gmail.com>
2026-01-26 20:09:02 +1100

    A greedy little blue fella

--- config/make.py
+++ config/make.py
@@ -1,37 +1,90 @@
+import FreeSimpleGUI as fsGUI
+fsGUI.theme("LightGrey1")
+
 def makeConfig(LANG_NAME: str, MAX_SOUND_LENGTH: str, SOUNDS: dict): # Compile the variables into the config format
     pass
 
-def importConfig(): # Paste from clipboard
-    pass
-
 def newConfig(): # Create new
-    print("Create New Language")
-    print("-------------------")
-
-    print("Language Name:")
-    LANG_NAME = input("> ")
+    def createNewConfigLayout():
+        return [[
+            fsGUI.Text("Create New Language"),
+            fsGUI.Button("Save and Continue")],
+            [fsGUI.InputText("Language Name:", key="-LANG_NAME-")],
+            [fsGUI.InputText("Longest Graph:", key="-MAX_SOUND_LENGTH-")],
+            [fsGUI.Button("<- Back")]
+        ]
     
-    print("Longest graph:")
-    MAX_SOUND_LENGTH = input("> ")
+    def createAddSoundsLayout():
+        return [[
+            fsGUI.Text(f"Add sounds to {langName}:")]
+            [fsGUI.InputText("Character(s) (Uppercase):", key="-CHAR_CAP-")],
+            [fsGUI.InputText("Character(s) (Lowercase):", key="-CHAR_LOWER-")],
+            [fsGUI.InputText("IPA Symbol(s):", key="-IPA_SYMBOL-")],
+            [fsGUI.Button("Add Sound"), fsGUI.Button("Finish")]
+        ]
 
-    done = False
-    SOUNDS = {}
-    while not done:
-        sound: list = []
-        print("                         Type STOP to stop adding sounds.")
-        print("Start of char/ipa pair.  Type the character(s), beginning with a capital:")
-        soundOrSTOP = input("> ")
-        if soundOrSTOP == "STOP":
-            done == True
-        sound.append(soundOrSTOP)
-
-        print("                         Type the character(s) in lowercase:")
-        sound.append(input("> "))
-
-        print("                         Type the IPA symbol(s):")
-        sound.append(input("> "))
-
-        SOUNDS[sound[0] + sound[1]] = sound[2]
+    window = fsGUI.Window("IPAt - Create New Language", createNewConfigLayout(), no_titlebar=True, grab_anywhere=True, keep_on_top=True)
+    while True:
+        event, values = window.read()
+        if event == fsGUI.WIN_CLOSED or event == "<- Back":
+            window.close()
+            break
+        if event == "Save and Continue":
+            langName: str = values["-LANG_NAME-"]
+            maxSoundLength: str = values["-MAX_SOUND_LENGTH-"]
+            soundsWindow = fsGUI.Window(f"IPAt - Add Sounds to {langName}", createAddSoundsLayout(), no_titlebar=True, grab_anywhere=True, keep_on_top=True)
+            window.close()
+            break
     
-    makeConfig(LANG_NAME, MAX_SOUND_LENGTH, SOUNDS)
+    sounds: dict = {}
+    addingSounds: bool = True
+    while addingSounds:
+        event, values = soundsWindow.read()
+
+        if event == "Add Sound":
+            charCap: str = values["-CHAR_CAP-"]
+            charLower: str = values["-CHAR_LOWER-"]
+            ipaSymbol: str = values["-IPA_SYMBOL-"]
+            sounds[charCap + charLower] = ipaSymbol
+
+            soundsWindow["-CHAR_CAP-"].update("")
+            soundsWindow["-CHAR_LOWER-"].update("")
+            soundsWindow["-IPA_SYMBOL-"].update("")
+        
+        if event == "Finish":
+            addingSounds = False
+            soundsWindow.close()
+            break
+    
+    makeConfig(langName, maxSoundLength, sounds)
+
+    # print("Create New Language")
+    # print("-------------------")
+
+    # print("Language Name:")
+    # LANG_NAME = input("> ")
+    
+    # print("Longest graph:")
+    # MAX_SOUND_LENGTH = input("> ")
+
+    # done = False
+    # SOUNDS = {}
+    # while not done:
+    #     sound: list = []
+    #     print("                         Type STOP to stop adding sounds.")
+    #     print("Start of char/ipa pair.  Type the character(s), beginning with a capital:")
+    #     soundOrSTOP = input("> ")
+    #     if soundOrSTOP == "STOP":
+    #         done == True
+    #     sound.append(soundOrSTOP)
+
+    #     print("                         Type the character(s) in lowercase:")
+    #     sound.append(input("> "))
+
+    #     print("                         Type the IPA symbol(s):")
+    #     sound.append(input("> "))
+
+    #     SOUNDS[sound[0] + sound[1]] = sound[2]
+    
+    # makeConfig(LANG_NAME, MAX_SOUND_LENGTH, SOUNDS)
 
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import pyperclip
 import FreeSimpleGUI as fsGUI
 fsGUI.theme("LightGrey1")
 
+storedLanguages: list = []
+
 def createHomeLayout():
     return [[fsGUI.Text("IPAt")],
             [fsGUI.Button("Import/Create a language config"),
@@ -21,6 +23,7 @@ def createIoCLayout():
 def main():
     window = fsGUI.Window("IPAt", createHomeLayout(), no_titlebar=True, grab_anywhere=True, keep_on_top=True)
 
+    # ----   Home   ----
     while True:
         event, values = window.read()
         if event == fsGUI.WIN_CLOSED or event == "Close":
@@ -29,13 +32,18 @@ def main():
 
         if event == "Import/Create a language config":
             IoCwindow = fsGUI.Window("IPAt", createIoCLayout(), no_titlebar=True, grab_anywhere=True, keep_on_top=True)
-
+            
+            # ----   IoC   ----
             while True:
                 IoCevent, IoCvalues = IoCwindow.read()
 
                 if IoCevent == "Import (Paste from clipboard)":
                     clipboardData = pyperclip.paste()
-                    
+                    try:
+                        storedLanguages.append(configTools.extractLanguage(clipboardData))
+                    except Exception as x:
+                       fsGUI.popup(f"Error importing config from clipboard: {x}", no_titlebar=True, keep_on_top=True, grab_anywhere=True)
+                
 
                 if IoCevent == "Import (Select File)":
                     pass
@@ -46,11 +54,13 @@ def main():
                 if IoCevent == fsGUI.WIN_CLOSED or IoCevent == "<- Back":
                     IoCwindow.close()
                     break
-
+            # ---- END IoC ----
+    # ---- END Home ----
 
 def testLine():
-    print(configTools.compileDefault(configTools.default))
-    print(configTools.extractLanguage(configTools.defaultCompiled))
+    # print(configTools.compileDefault(configTools.default))           #  (Works)
+    # print(configTools.extractLanguage(configTools.defaultCompiled))  #  (Works)
+    pass
 
 if __name__ == "__main__":
     main()
